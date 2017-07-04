e9ff5d6766dac24e54bf0763311912e351234b52
chendongyu0125 <chendongyu0125@gmail.com>
2017-07-04 23:52:01 +0800

    make changes
    add some codes here

--- DB.py
+++ DB.py
@@ -1,2 +1,10 @@
 import pandas as pd
+import pymysql
+from sqlalchemy import create_engine
+from bs4 import BeautifulSoup as bs
+from nltk.tokenize import RegexpTokenizer as rt
 import numpy as np
+e=create_engine("mysql+pymysql://chendongyu:1234@localhost/ppdai?charset=utf8")
+
+member=pd.read_sql_table("member",e)
+print(member.columns)
